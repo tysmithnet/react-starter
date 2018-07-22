5ba9e5ec4de01effb6d85a55df51084e0a063f95
Ty Smith <dts5004@gmail.com>
2018-07-22 02:11:36 -0700

    Added additional dependencies for storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,6 +4,8 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+import App from "../src/App";
+
 import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
