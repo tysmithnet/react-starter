8e45ecb5235402ae562fb398bacbc863028ebee5
Ty Smith <dts5004@gmail.com>
2018-07-22 14:42:09 -0700

    Added hello world saga Added redux and saga

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import * as React from "react";
+import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from "./sagas";
+
+export function reducer(state: any, action: any) {
+    console.log("reducer called");
+}
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
+
 
 export default class App extends React.Component <{}, {}>{
     render() {
         return <h1>hi!</h1>
     }
-}
+}4
