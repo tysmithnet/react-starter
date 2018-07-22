974a93940a88fcf2f26d41e4d2ff062f59abaaad
Ty Smith <dts5004@gmail.com>
2018-07-22 01:43:09 -0700

    Locked down dependency versions Used jest's built it code coverage functionality Made dev server, unit tests, and e2e tests work together Fixed compatibility issue between jest and cypress Added cypress for e2e testing Updated README Refactored config files Added jest and hello world test

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
+import App from "./App";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
-export class App extends React.Component <{}, {}>{
-    render() {
-        return <h1>hi!</h1>
-    }
-}
 
 ReactDOM.render(<App />, document.getElementById("root"));
