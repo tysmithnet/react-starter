823fc00b0537bdf0d4ae19ccc42050a02d07bb30
Ty Smith <dts5004@gmail.com>
2018-07-22 16:46:47 -0700

    General cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { applyMiddleware, createStore } from "redux";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas";
-import something from "./something";
 
 export function reducer(state: any, action: any) {
   console.log("reducer called");
@@ -12,6 +11,9 @@ const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(rootSaga);
 
+/**
+ * @description The root component
+ */
 export default class App extends React.Component<{}, {}> {
   public render() {
     return <h1>hi!</h1>;
--- src/sagas.ts
+++ src/sagas.ts
@@ -1,16 +1,14 @@
-import { delay } from 'redux-saga'
-import { put, takeEvery, all } from 'redux-saga/effects'
+import { delay } from "redux-saga";
+import { all } from "redux-saga/effects";
 
-export function * helloSaga() {
-    console.log("wait...")
-    yield delay(1000);
-    console.log("Hello")
+export function* helloSaga() {
+  console.log("wait...");
+  yield delay(1000);
+  console.log("Hello");
 }
 
 // notice how we now only export the rootSaga
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
-  yield all([
-    helloSaga()
-  ])
+  yield all([helloSaga()]);
 }
