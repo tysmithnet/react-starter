3628aba9013d94b8fe11b768190b6137624d0c1a
Ty Smith <dts5004@gmail.com>
2018-08-02 12:45:03 -0700

    Added first route for Home

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,18 @@ import { ConnectedRouter } from "connected-react-router";
 import * as React from "react";
 import { hot } from "react-hot-loader";
 import { connect } from "react-redux";
+import { Route } from "react-router";
+import Home from "../home/Home";
 import { IRootState } from "../root.state";
 import {history} from "../root.store";
 import { IProps } from "./domain";
 
 export class App extends React.Component<IProps> {
   public render() {
-
+// add routes
     return (
       <ConnectedRouter history={history}>
-        {null}
+        <Route exact={true} path="/" component={Home}/>
       </ConnectedRouter>
     );
   }
