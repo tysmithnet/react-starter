9f83735346333c51651299e31b85bc11b2bf9504
Ty Smith <dts5004@gmail.com>
2018-08-27 01:24:20 -0700

    Added comments

--- src/auth/auth.domain.ts
+++ src/auth/auth.domain.ts
@@ -7,18 +7,27 @@ export interface IRootState {
   user: IUser;
 }
 
+/**
+ * Represents access control condition
+ */
 export interface IPermission {
   id: string;
   name: string;
   description?: string;
 }
 
+/**
+ * Represents a user in the system
+ */
 export interface IUser {
   id: string;
   name: string;
   permissions: IPermission[];
 }
 
+/**
+ * Collection of all permissions
+ */
 const permissions: IPermission[] = [
   {
     description: "Can manage admin settings",
@@ -27,15 +36,11 @@ const permissions: IPermission[] = [
   },
 ];
 
+/**
+ * Keyed collection of all permissions
+ */
 export const Permissions: Map<string, IPermission> = new Map<
   string,
   IPermission
 >();
 permissions.forEach(p => Permissions.set(p.id, p));
-
-export type IProps = IBaseProps;
-
-export interface IState {
-  id?: string;
-  password?: string;
-}
--- src/auth/auth.saga.ts
+++ src/auth/auth.saga.ts
@@ -3,6 +3,11 @@ import { all, put, takeLatest } from "redux-saga/effects";
 import { ACTION_TYPES, ILoginRequest } from "./auth.action";
 import { Permissions } from "./auth.domain";
 
+/**
+ * Attempt to login using the provided credentials
+ * @param id Who to log in as
+ * @param password Password for id
+ */
 function* loginUser(id: string, password: string) {
   try {
     const res = yield axios.post("/api/auth", {
@@ -33,12 +38,18 @@ function* loginUser(id: string, password: string) {
   }
 }
 
+/**
+ * Saga responding to login requests
+ */
 export function* loginSaga() {
   yield takeLatest(ACTION_TYPES.LOGIN_REQUEST, (action: ILoginRequest) => {
     return loginUser(action.id, action.password);
   });
 }
 
+/**
+ * Root saga for Auth domain
+ */
 export function* rootSaga() {
   yield all([loginSaga()]);
 }
