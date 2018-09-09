3c6c9eb31849aaaac77eef418d82d57de17d1465
Ty Smith <dts5004@gmail.com>
2018-09-08 22:57:56 -0700

    General cleanup

--- src/root.tsx
+++ src/root.tsx
@@ -1,35 +1,25 @@
+import axios from "axios";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
+import { observe } from "react-performance-observer";
 import { Provider } from "react-redux";
 import App from "./app/App";
 import rootSaga from "./root.saga";
 import store, { sagaMiddleware } from "./root.store";
-import { observe } from 'react-performance-observer';
-import axios from "axios";
 
+// register a metric tracking routine
 observe(measurements => {
-  for(let measurement of measurements) {
-    if(measurement.entryType != "measure")
+  for (const measurement of measurements) {
+    if (measurement.entryType != "measure") {
       continue;
+    }
     axios.post("/metrics", {
-      data: `${measurement.componentName} - ${measurement.duration}`
-    })
+      data: `${measurement.componentName} - ${measurement.duration}`,
+    });
   }
-  console.log(measurements);
-  // [
-  //   {
-  //     entryType: "measure",
-  //     name: "⚛ App [mount]",
-  //     componentName: "App",
-  //     phase: "mount",
-  //     startTime: 281,
-  //     duration: 4,
-  //     warning: null
-  //   },
-  //   ...
-  // ]
 });
+
 /**
  * Render a component, connecting to it the root store
  * @param component Component to provide state for
