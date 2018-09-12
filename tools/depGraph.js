53d5a15a3915c5f079c7aa7cf3b434c6cecbee97
Ty Smith <dts5004@gmail.com>
2018-09-12 00:12:35 -0700

    Rewrote dependency graph generation madge was broken

--- tools/depGraph.js
+++ tools/depGraph.js
@@ -1,16 +1,10 @@
 const madge = require("madge");
 const path = require("path");
 const find = require("find");
+const colony = require("colony");
 
-const config = {
-    includeNpm: true,
-    webpackConfig: path.resolve("config/webpack.config.js")
-}
-
-const files = find.file(/\.[jt]sx?$/, path.resolve("src"), (files) => {
-    madge(files, config)
-    .then((res) => res.image(path.resolve("depGraph.png")))
-    .then((writtenImagePath) => {
-        console.log("Dependency Graph Written to:  " + writtenImagePath);
+find.file(/\.[jt]sx?$/, path.resolve("src"), (files) => {
+    colony.build(files, {}, () => {
+        console.log("Complete");
     });
 });
