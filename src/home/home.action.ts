c8b0a1df694e12efd633bc6a4a2ad519f3bc3347
Ty Smith <dts5004@gmail.com>
2018-09-14 01:59:28 -0700

    Fixed naming convention for home actions

--- src/home/home.action.ts
+++ src/home/home.action.ts
@@ -4,8 +4,8 @@ import { IAction } from "../root.reducer";
  * Actions for the Home domain
  */
 export const ACTION_TYPES = {
-  START_ANIMATION_REQUEST: "@Home/StartAnimationRequest",
-  START_ANIMATION_SUCCESS: "@Home/StartAnimationSuccess",
+  START_ANIMATION_REQUEST: "@home/StartAnimationRequest",
+  START_ANIMATION_SUCCESS: "@home/StartAnimationSuccess",
 };
 
 /**
