c8b4fd11be5983b46024b39175219bb7a98e3312
Tyler Smith <dts5004@gmail.com>
2020-02-09 01:07:00 -0800

    Removed App module

--- src/app/App.tsx
+++
@@ -1,8 +0,0 @@
-  
-import * as React from 'react';
-
-export const App: React.SFC = _ => (
-    <div>
-        <h1>hi!</h1>
-    </div>
-);
--- src/root/root.tsx
+++ src/root/root.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { App } from "../app";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>hi</h1>, document.getElementById('root'));
