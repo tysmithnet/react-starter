755608ddb194f53285549a0fa8de337f95d7da81
Tyler Smith <dts5004@gmail.com>
2020-02-16 23:38:47 -0800

    Add express as development server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const webpack = require("webpack");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+
+const app = express();
+const config = require("./webpack.config.dev");
+const compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, { publicPath: config.output.publicPath }));
+
+app.listen(3000, () => {
+    console.log("Listing on :3000");
+})
