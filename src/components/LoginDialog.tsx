62413679e094557ea6d6394bb5c0f39ca8ef3d4f
Ty Smith <dts5004@gmail.com>
2020-02-19 23:40:44 -0800

    Migrate LoginDialog to functional pattern
    We no longer want to be using class based components anywhere.

--- src/components/LoginDialog.tsx
+++ src/components/LoginDialog.tsx
@@ -22,105 +22,59 @@ export interface LoginDialogState {
     password: string;
 }
 
-/**
- *
- *
- * @param {LoginDialogProps} props
- * @param {LoginDialogState} state
- * @returns
- */
-export class LoginDialog extends React.Component<LoginDialogProps, LoginDialogState> {
-    public props: LoginDialogProps;
-    public state: LoginDialogState;
+export function LoginDialog(props: LoginDialogProps) {
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
 
-    /**
-     *Creates an instance of LoginDialog.
-     * @param {*} props
-     * @param {*} state
-     * @memberof LoginDialog
-     */
-    constructor(props, state) {
-        super(props);
-        this.props = props;
-        this.state = {
-            username: '',
-            password: '',
-        };
-        this.handleUsernameChanged = this.handleUsernameChanged.bind(this);
-        this.handlePasswordChanged = this.handlePasswordChanged.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = () => {
+        props.onSubmit(username, password);
+    };
+
+    let error = null;
+    if (props.errorMessage) {
+        error = <Typography color="error">{props.errorMessage}</Typography>;
     }
 
-    /**
-     * Handle the user id input changed
-     * @param event Input changed event
-     */
-    private handleUsernameChanged(event: React.ChangeEvent<HTMLInputElement>) {
-        this.setState({ ...this.state, username: event.target.value });
-    }
-
-    /**
-     * Handle the user password input changed
-     * @param event Input changed event
-     */
-    private handlePasswordChanged(event: React.ChangeEvent<HTMLInputElement>) {
-        this.setState({ ...this.state, password: event.target.value });
-    }
-
-    /**
-     *
-     *
-     * @private
-     * @memberof LoginDialog
-     */
-    private handleSubmit() {
-        this.props.onSubmit(this.state.username, this.state.password);
-    }
-
-    /**
-     *
-     *
-     * @returns
-     * @memberof LoginDialog
-     */
-    public render() {
-        let error = null;
-        if (this.props.errorMessage) {
-            error = <Typography color="error">{this.props.errorMessage}</Typography>;
-        }
-        return (
-            <Dialog open={this.props.open} onClose={this.props.onClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>Please enter your username and password to login</DialogContentText>
-                    {error}
-                    <TextField
-                        autoFocus
-                        onChange={this.handleUsernameChanged}
-                        margin="dense"
-                        id="logindialog-username"
-                        label="Username"
-                        type="text"
-                        fullWidth
-                    />
-                    <TextField
-                        onChange={this.handlePasswordChanged}
-                        margin="dense"
-                        id="logindialog-password"
-                        label="Password"
-                        type="password"
-                        fullWidth
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={this.handleSubmit} color="primary">
-                        Login
-                    </Button>
-                    <Button onClick={this.props.onClose} color="secondary">
-                        Cancel
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        );
-    }
+    return (
+        <Dialog open={props.open} onClose={props.onClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Login</DialogTitle>
+            <DialogContent>
+                <DialogContentText>Please enter your username and password to login</DialogContentText>
+                {error}
+                <TextField
+                    autoFocus
+                    onChange={handleUsernameChange}
+                    margin="dense"
+                    id="logindialog-username"
+                    label="Username"
+                    type="text"
+                    fullWidth
+                />
+                <TextField
+                    onChange={handlePasswordChange}
+                    margin="dense"
+                    id="logindialog-password"
+                    label="Password"
+                    type="password"
+                    fullWidth
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleSubmit} color="primary">
+                    Login
+                </Button>
+                <Button onClick={props.onClose} color="secondary">
+                    Cancel
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
 }
