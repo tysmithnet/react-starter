b0333fa264e095ed117549fb2aa6625c710f0de8
Ty Smith <dts5004@gmail.com>
2020-02-19 22:58:58 -0800

    Make App functional component
    We will be abandoning the old style of using class-based components in favor of functional ones.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -37,15 +37,17 @@ import PeopleIcon from '@material-ui/icons/People';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { Action, history, State } from '../root';
 import { User } from '../user';
-import { Action, configureStore, history, State } from '../root';
 import { connect, Provider } from 'react-redux';
-import { createMuiTheme, makeStyles, Theme, ThemeProvider, withTheme, useTheme, withStyles } from '@material-ui/core/styles';
+import { createMuiTheme, makeStyles, Theme, ThemeProvider } from '@material-ui/core/styles';
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@material-ui/core';
 import { loginRequestFactory } from '../auth';
-import { Props } from '../root';
-import { drawerChangeRequestFactory, loginDialogChangeRequestFactory } from '.';
-
+/**
+ * Displays the copyright for the page
+ *
+ * @returns
+ */
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
@@ -108,210 +110,236 @@ const secondaryListItems = (
     </div>
 );
 
-interface AppProps extends Props {
-    currentTheme: AppTheme;
-    drawerOpen: boolean;
-    loginOpen: boolean;
+interface AppProps {
     currentUser: User;
+    dispatch?: (action: Action) => void;
 }
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    toolbar: {
+        paddingRight: 24, // keep right padding when drawer closed
+    },
+    toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    menuButtonHidden: {
+        display: 'none',
+    },
+    title: {
+        flexGrow: 1,
+    },
+    drawerPaper: {
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerPaperClose: {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9),
+        },
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+    },
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+    fixedHeight: {
+        height: 240,
+    },
+}));
 
+/**
+ * The Root of the component tree
+ *
+ */
+function AppRaw(props: AppProps) {
+    const classes = useStyles();
+    const [drawerOpen, setDrawerOpen] = React.useState(true);
+    const [loginOpen, setLoginOpen] = React.useState(false);
 
-class AppInternal extends React.Component<AppProps, {}> {
-    public props: AppProps;
-    public state: {};
-    public classes: any;
+    /**
+     * Handle requests to open the drawer
+     *
+     */
+    const handleDrawerOpen = () => {
+        setDrawerOpen(true);
+    };
 
-    public useStyles = makeStyles(theme => ({
-        root: {
-            display: 'flex',
-        },
-        toolbar: {
-            paddingRight: 24, // keep right padding when drawer closed
-        },
-        toolbarIcon: {
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            padding: '0 8px',
-            ...theme.mixins.toolbar,
-        },
-        appBar: {
-            zIndex: theme.zIndex.drawer + 1,
-            transition: theme.transitions.create(['width', 'margin'], {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-        },
-        appBarShift: {
-            marginLeft: drawerWidth,
-            width: `calc(100% - ${drawerWidth}px)`,
-            transition: theme.transitions.create(['width', 'margin'], {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-        },
-        menuButton: {
-            marginRight: 36,
-        },
-        menuButtonHidden: {
-            display: 'none',
-        },
-        title: {
-            flexGrow: 1,
-        },
-        drawerPaper: {
-            position: 'relative',
-            whiteSpace: 'nowrap',
-            width: drawerWidth,
-            transition: theme.transitions.create('width', {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-        },
-        drawerPaperClose: {
-            overflowX: 'hidden',
-            transition: theme.transitions.create('width', {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-            width: theme.spacing(7),
-            [theme.breakpoints.up('sm')]: {
-                width: theme.spacing(9),
-            },
-        },
-        appBarSpacer: theme.mixins.toolbar,
-        content: {
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-        },
-        container: {
-            paddingTop: theme.spacing(4),
-            paddingBottom: theme.spacing(4),
-        },
-        paper: {
-            padding: theme.spacing(2),
-            display: 'flex',
-            overflow: 'auto',
-            flexDirection: 'column',
-        },
-        fixedHeight: {
-            height: 240,
-        },
-    }));
+    /**
+     * Handle requests to close the drawer
+     *
+     */
+    const handleDrawerClose = () => {
+        setDrawerOpen(false);
+    };
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
-    constructor(props: AppProps) {
-        super(props);
-        this.props = props;
-        this.state = {};
-        this.classes = this.useStyles();
-        this.handleDrawerChange = this.handleDrawerChange.bind(this);
-        this.handleLoginChange = this.handleLoginChange.bind(this);
-        this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
-        this.handleToggleLightDarkMode = this.handleToggleLightDarkMode.bind(this);
-    }
+    /**
+     *
+     *
+     */
+    const handleToggleLightDarkMode = () => {
+        //props.dispatch(changeThemeRequestFactory(props.theme, null, true));
+    };
 
-    public handleDrawerChange() {
-        this.props.dispatch(drawerChangeRequestFactory(!this.props.drawerOpen));
-    }
+    /**
+     *
+     *
+     */
+    const handleLoginClose = () => {
+        setLoginOpen(false);
+    };
 
-    public handleLoginChange() {
-        this.props.dispatch(loginDialogChangeRequestFactory(!this.props.loginOpen));
-    }
+    /**
+     *
+     *
+     */
+    const handleLoginOpen = () => {
+        setLoginOpen(true);
+    };
 
-    public handleLoginSubmit(username: string, password: string) {
-        this.props.dispatch(loginRequestFactory(username, password));
-    }
+    /**
+     *
+     *
+     * @param {string} username
+     * @param {string} password
+     */
+    const handleLoginSubmit = (username: string, password: string) => {
+        const request = loginRequestFactory(username, password);
+        props.dispatch(request);
+    };
 
-    public handleToggleLightDarkMode() {
-        this.props.dispatch(changeThemeRequestFactory(this.props.currentTheme, null, true));
-    }
-
-    public render() {
-        const avatar = null;
-        return (
-            <div className={this.classes.root}>
-                <CssBaseline />
-                <AppBar
-                    position="absolute"
-                    className={clsx(this.classes.appBar, this.props.drawerOpen && this.classes.appBarShift)}
-                >
-                    <Toolbar className={this.classes.toolbar}>
-                        <IconButton
-                            edge="start"
-                            color="inherit"
-                            aria-label="open drawer"
-                            onClick={this.handleDrawerChange}
-                            className={clsx(
-                                this.classes.menuButton,
-                                this.props.drawerOpen && this.classes.menuButtonHidden,
-                            )}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography component="h1" variant="h6" color="inherit" noWrap className={this.classes.title}>
-                            React Starter
-                        </Typography>
-                        <IconButton color="inherit" onClick={this.handleToggleLightDarkMode}>
-                            <Brightness4Icon />
-                        </IconButton>
-                        <IconButton color="inherit">
-                            <Badge badgeContent={4} color="secondary">
-                                <NotificationsIcon />
-                            </Badge>
-                        </IconButton>
-                        {avatar}
-                    </Toolbar>
-                </AppBar>
-                <Drawer
-                    variant="permanent"
-                    classes={{
-                        paper: clsx(this.classes.drawerPaper, !this.props.drawerOpen && this.classes.drawerPaperClose),
-                    }}
-                    open={this.props.drawerOpen}
-                >
-                    <div className={this.classes.toolbarIcon}>
-                        <IconButton onClick={this.handleDrawerChange}>
-                            <ChevronLeftIcon />
-                        </IconButton>
-                    </div>
-                    <Divider />
-                    <List>{mainListItems}</List>
-                    <Divider />
-                    <List>{secondaryListItems}</List>
-                </Drawer>
-                <main className={this.classes.content}>
-                    <div className={this.classes.appBarSpacer} />
-                    <Container maxWidth="lg" className={this.classes.container}>
-                        <Grid container spacing={3}>
-                            <Grid item xs={12} md={12} lg={12}>
-                                <ConnectedRouter history={history} />
-                            </Grid>
-                        </Grid>
-                        <Box pt={4}>
-                            <Copyright />
-                        </Box>
-                    </Container>
-                </main>
-                <LoginDialog
-                    open={this.props.loginOpen}
-                    onClose={this.handleLoginChange}
-                    onSubmit={this.handleLoginSubmit}
-                />
-            </div>
+    let avatar = null;
+    if (props.currentUser) {
+        const alt = `${props.currentUser.fname} ${props.currentUser.lname}`;
+        avatar = <Avatar alt={alt} src={props.currentUser.image} />;
+    } else {
+        avatar = (
+            <IconButton color="inherit">
+                <AccountIcon onClick={handleLoginOpen} />
+            </IconButton>
         );
     }
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="absolute" className={clsx(classes.appBar, drawerOpen && classes.appBarShift)}>
+                <Toolbar className={classes.toolbar}>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        className={clsx(classes.menuButton, drawerOpen && classes.menuButtonHidden)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                        React Starter
+                    </Typography>
+                    <IconButton color="inherit" onClick={handleToggleLightDarkMode}>
+                        <Brightness4Icon />
+                    </IconButton>
+                    <IconButton color="inherit">
+                        <Badge badgeContent={4} color="secondary">
+                            <NotificationsIcon />
+                        </Badge>
+                    </IconButton>
+                    {avatar}
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                classes={{
+                    paper: clsx(classes.drawerPaper, !drawerOpen && classes.drawerPaperClose),
+                }}
+                open={drawerOpen}
+            >
+                <div className={classes.toolbarIcon}>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>{mainListItems}</List>
+                <Divider />
+                <List>{secondaryListItems}</List>
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.appBarSpacer} />
+                <Container maxWidth="lg" className={classes.container}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} md={12} lg={12}>
+                            <ConnectedRouter history={history} />
+                        </Grid>
+                    </Grid>
+                    <Box pt={4}>
+                        <Copyright />
+                    </Box>
+                </Container>
+            </main>
+            <LoginDialog open={loginOpen} onClose={handleLoginClose} onSubmit={handleLoginSubmit} />
+        </div>
+    );
 }
 
+/**
+ *
+ *
+ * @param {State} state
+ * @returns {AppProps}
+ */
 function mapStateToProps(state: State): AppProps {
     return {
-        currentTheme: state.theme,
-        currentUser: state?.auth?.user,
-        loginOpen: state?.app?.ui?.loginOpen ?? false,
-        drawerOpen: state?.app?.ui?.drawerOpen ?? true,
+        currentUser: null,
     };
 }
 
-const connectedApp = connect(mapStateToProps)(AppInternal);
-export const App = connectedApp;
+export const App = connect(mapStateToProps)(AppRaw);
