fa31f8f106b3b0d11109e1f55783fcb88935cd78
Ty Smith <dts5004@gmail.com>
2020-02-21 03:08:08 -0800

    Add example of nested routing
    This demonstrates how top level routes can delegate domain routing to their own top level components. e.g. the project listing component can have a place on the main menu, but the project details can can be handled by the projects domain.

--- src/project/ProjectListing.tsx
+++ src/project/ProjectListing.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+
+export function ProjectDescription() {
+    return <h1>PROJECT DESCRIPTION</h1>;
+}
 
 export function ProjectListing() {
-    return <h1>PROJECT!</h1>;
+    return (
+        <div>
+            <h1>PROJECT LISTING!</h1>
+            <Link to="/projects/1">Project 1</Link>
+            <Route path="/projects/:projectId" component={ProjectDescription} />
+        </div>
+    );
 }
--- src/project/project.route.tsx
+++ src/project/project.route.tsx
@@ -6,10 +6,10 @@ import FolderOpen from '@material-ui/icons/FolderOpen';
 export const ProjectListingRoute: RouteData = { 
     component: ProjectListing,
     display: 'Projects',
-    exact: true,
+    exact: false,
     icon: <FolderOpen />,
     id: '4cbd7807-2a54-478b-9132-345d084cc0c5',
-    path: '/project',
+    path: '/projects',
     permissions: [],
     type: 'main'
 };
